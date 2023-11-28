8ee98b5ed686c14b56bd546872ed6349e4ea3c17
PallaviRao1113 <palche1113@gmail.com>
2023-11-28 16:07:21 -0500

    Pushing US01 code, and a few changes to the manifest and gradle files.

--- app/src/main/java/com/example/wordsearchapplication_mobileappdev/MainActivity.java
+++ app/src/main/java/com/example/wordsearchapplication_mobileappdev/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.wordsearchapplication_mobileappdev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gotoSecondActivty (View v)
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
 }
