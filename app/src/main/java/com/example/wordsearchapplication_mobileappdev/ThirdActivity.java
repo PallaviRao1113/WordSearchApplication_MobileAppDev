401e55c3a6e3b0dfdd14ed4ba9d40d691a831250
Tyree <tyreerussellmccloud@gmail.com>
2023-11-30 15:52:44 -0500

    if three letter word is found in easy mode it goes to main Activity,

--- app/src/main/java/com/example/wordsearchapplication_mobileappdev/ThirdActivity.java
+++ app/src/main/java/com/example/wordsearchapplication_mobileappdev/ThirdActivity.java
@@ -3,15 +3,19 @@ package com.example.wordsearchapplication_mobileappdev;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
+
+import java.util.Arrays;
 import java.util.Random;
 import com.example.wordsearchapplication_mobileappdev.CountDownTimerView;
 
 public class ThirdActivity extends AppCompatActivity {
     private static final int GRID_SIZE = 3;
+    private StringBuilder selectedLetters = new StringBuilder();
     private GridLayout gridLayout;
     public CountDownTimerView timerr;
 
@@ -57,6 +61,7 @@ public class ThirdActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ThirdActivity.this, "Button Clicked: " + buttonText, Toast.LENGTH_SHORT).show();
+                handleLetterButtonClick(buttonText);
             }
         });
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
@@ -67,7 +72,39 @@ public class ThirdActivity extends AppCompatActivity {
         gridLayout.addView(button);
     }
 
-    public void gotoMainActivity (View v)
+    private void handleLetterButtonClick(String letter) {
+        selectedLetters.append(letter);
+
+        // Log the clicked letter
+        Log.d("ButtonClicked", "Clicked Letter: " + letter);
+
+        if (selectedLetters.length() == 3) {
+            String selectedWord = selectedLetters.toString().toLowerCase();
+            Log.d("WordCheck", "Selected Word: " + selectedWord);
+
+            if (isWord(selectedWord)) {
+                Log.d("WordCheck", "Valid Word! Navigating to MainActivity");
+                // Word is valid, navigate to the desired activity
+                // CHANGE THIS TO GO TO POINT SYSTEM
+                gotoMainActivity();
+            } else {
+                Log.d("WordCheck", "Not a valid word");
+            }
+
+            // Clear selected letters for the next word
+            selectedLetters.setLength(0);
+        }
+    }
+
+
+    private boolean isWord(String selectedWord) {
+        // Implement your logic to check if the selected word is valid
+        // For simplicity, let's check against a predefined list of words
+        String[] validWords = {"cat", "dog", "cow", "sun", "job", "dad", "mom", "kid", "car", "rug", "boy", "two", "day"};
+        return Arrays.asList(validWords).contains(selectedWord);
+    }
+
+    public void gotoMainActivity ()
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
